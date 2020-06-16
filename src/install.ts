7ca38ecf928c29c4c99d8cebf982010b2bc184f5
Rishabh Arya <rishabh.aryain@gmail.com>
2020-06-16 19:35:57 +0530

    enable js actions

--- src/install.ts
+++ src/install.ts
@@ -1,18 +1,37 @@
 import * as tc from "@actions/tool-cache";
+import os from "os";
+import fs from "fs";
+import path from "path";
+import * as exec from "@actions/exec";
 
-// https://downloads.lambdatest.com/tunnel/windows/64bit/ltcomponent.zip
-// https://downloads.lambdatest.com/tunnel/linux/64bit/ltcomponent.zip
-// https://downloads.lambdatest.com/tunnel/mac/64bit/ltcomponent.zip
-//
 export async function downloadBinary() {
+  let cachedPath: string;
 
-  let downloadUrl: string = `https://downloads.lambdatest.com/tunnel/${getPlatform()}/64bit/ltcomponent.zip`;
-  console.log(`Downloading from ${downloadUrl}`);
+  try {
+    let downloadUrl: string = `https://downloads.lambdatest.com/tunnel/${getPlatform()}/64bit/ltcomponent.zip`;
+    console.log(`Downloading from ${downloadUrl}`);
 
+    const tunnelZip = await tc.downloadTool(downloadUrl);
+    const tunnelExtracted = await tc.extractZip(tunnelZip);
+
+    cachedPath = await tc.cacheFile(
+      path.join(tunnelExtracted, "ltcomponent"),
+      "LT",
+      "tunnel",
+      "v2"
+    );
+
+    fs.chmodSync(path.join(cachedPath,"LT"), "0755");
+
+  } catch (error) {
+    throw new Error(`Failed to download tunnel: ${error}`);
+  }
+
+  return path.join(cachedPath,"LT");
 }
 
 function getPlatform() {
-  switch (process.platform) {
+  switch (os.platform()) {
     case "darwin":
       return "mac";
     case "win32":
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,66 @@
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
-import getPort from 'get-port'
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import getPort from "get-port";
+import childProcess from 'child_process';
+import * as installer from "./install";
 
-export async function run(){
-    
+export async function run() {
+  try {
+    const tunnelBinaryPath: string = await installer.downloadBinary();
+    const tunnelParams: Array<string> = await getTunnelParams();
+    const options: exec.ExecOptions = {};
+
+    let myOutput = "";
+    let myError = "";
+
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        myOutput += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        myError += data.toString();
+      },
+    };
+
+    const tunnelProcess = childProcess.spawn(tunnelBinaryPath, tunnelParams, {
+        shell: true,
+        detached: true,
+      });
+      tunnelProcess.stdout.on('data', data => {
+       console.log(data.toString())
+      });
+      tunnelProcess.stderr.on('data', data => {
+        console.log(data.toString())
+      });
+      tunnelProcess.unref()
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function getTunnelParams() {
+  let params = [];
+
+  if (core.getInput("user")) params.push("--user", core.getInput("user"));
+  if (core.getInput("accessKey"))
+    params.push("--key", core.getInput("accessKey"));
+  if (core.getInput("tunnelName"))
+    params.push("--tunnelName", core.getInput("tunnelName"));
+  if (core.getInput("proxyHost"))
+    params.push("--proxy-host", core.getInput("proxyHost"));
+  if (core.getInput("proxyPort"))
+    params.push("--proxy-port", core.getInput("proxyPort"));
+  if (core.getInput("proxyUser"))
+    params.push("--proxy-user", core.getInput("proxyUser"));
+  if (core.getInput("proxyPass"))
+    params.push("--proxy-pass", core.getInput("proxyPass"));
+  if (core.getInput("sharedTunnel")) params.push("--shared-tunnel");
+  if (core.getInput("verbose")) params.push("-v");
+
+  let port = await getPort();
+  core.saveState("port", port);
+  params.push("--controller", "github", "--infoAPIPort", `${port}`);
+
+  return params;
 }
