5706c3f394c7fc2155925f710fc6175880e3033f
Mayank Bhola <mayankbhola@gmail.com>
2020-06-21 17:37:04 +0530

    Wrap params in double quotes

--- src/main.ts
+++ src/main.ts
@@ -50,19 +50,19 @@ async function getTunnelParams(port: Number) {
     params.push("--key", core.getInput("accessKey"));
   }
   if (core.getInput("tunnelName")) {
-    params.push("--tunnelName", core.getInput("tunnelName"));
+    params.push("--tunnelName", `"${core.getInput("tunnelName")}"`);
   }
   if (core.getInput("proxyHost")) {
-    params.push("--proxy-host", core.getInput("proxyHost"));
+    params.push("--proxy-host", `"${core.getInput("proxyHost")}"`);
   }
   if (core.getInput("proxyPort")) {
-    params.push("--proxy-port", core.getInput("proxyPort"));
+    params.push("--proxy-port", `"${core.getInput("proxyPort")}"`);
   }
   if (core.getInput("proxyUser")) {
-    params.push("--proxy-user", core.getInput("proxyUser"));
+    params.push("--proxy-user", `"${core.getInput("proxyUser")}"`);
   }
   if (core.getInput("proxyPass")) {
-    params.push("--proxy-pass", core.getInput("proxyPass"));
+    params.push("--proxy-pass", `"${core.getInput("proxyPass")}"`);
   }
   if (core.getInput("sharedTunnel")) {
     params.push("--shared-tunnel");
@@ -71,7 +71,7 @@ async function getTunnelParams(port: Number) {
     params.push("--ingress-only");
   }
   if (core.getInput("dns")) {
-    params.push("--dns", core.getInput("dns"));
+    params.push("--dns", `"${core.getInput("dns")}"`);
   }
   if (core.getInput("verbose")) {
     params.push("-v");
